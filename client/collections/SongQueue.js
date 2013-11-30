9103585b07e9a54421716ba882f3b6f7d1ee2fe2
Mr. Spothawk <mr.spothawk@gmail.com>
2013-11-29 20:22:35 -0800

    after removing item during ended method, checks to see if there are any more songs to play before invoking playFirst method.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -7,11 +7,13 @@ MyTunes.Collections.SongQueue = MyTunes.Collections.Songs.extend({
   initialize: function(){
     this.on('add', function(){
       if (this.length === 1)
-      this.playFirst();
+        this.playFirst();
     });
     this.on('ended', function(){
       this.remove(this.at(0));
-      this.playFirst();
+      if (this.at(0)) {
+        this.playFirst();
+      }
     })
   },
 
